85e29b9d7d668ab082684b04b0c5ca62a42093f1
jzhuk <julettazhuk@gmail.com>
2017-11-28 16:23:16 +0700

    some documentation added, reworked methods

--- annotation.py
+++ annotation.py
@@ -10,45 +10,64 @@ class Annotation:
         self.new_parent = None
         self.has_text = False
         self.annotation_text = None
+        self.parent_text = None
+        self.annotation_tail = None
+        self.annotation_head = None
 
     def set_new_parent(self, new_parent):
         self.new_parent = new_parent
 
     def get_text(self):
-        return get_text(self.parent_node)
+        if self.parent_text is not None:
+            return self.parent_text
+        else:
+            self.parent_text = get_text(self.parent_node)
+        return self.parent_text
 
     def get_annotation_text(self):
-        string = ''
-        if self.annotation_node.tail:
-            string += self.annotation_node.tail
-        sibling = self.annotation_node.getnext()
-        while sibling is not None and sibling.tag != '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}annotation-end':
-            string += get_text(sibling)
-            sibling = sibling.getnext()
-        return string
+        if self.annotation_text is not None:
+            return self.annotation_text
+        else:
+            string = ''
+            if self.annotation_node.tail:
+                string += self.annotation_node.tail
+            sibling = self.annotation_node.getnext()
+            while sibling is not None and sibling.tag != '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}annotation-end':
+                string += get_text(sibling)
+                sibling = sibling.getnext()
+            self.annotation_text = string
+        return self.annotation_text
 
     def get_annotation_tail(self):
         if self.has_text:
             return None
-        string = ''
-        if self.annotation_node.tail:
-            string += self.annotation_node.tail
-        sibling = self.annotation_node.getnext()
-        while sibling is not None:
-            string += get_text(sibling)
-            sibling = sibling.getnext()
-        return string
+        if self.annotation_tail is not None:
+            return self.annotation_tail
+        else:
+            string = ''
+            if self.annotation_node.tail:
+                string += self.annotation_node.tail
+            sibling = self.annotation_node.getnext()
+            while sibling is not None:
+                string += get_text(sibling)
+                sibling = sibling.getnext()
+            self.annotation_tail = string
+        return self.annotation_tail
 
     def get_annotation_head(self):
-        string = ''
-        if self.parent_node.text:
-            string += self.parent_node.text
-        children = self.parent_node.getchildren()
-        for child in children:
-            if child.tag == '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}annotation':
-                break
-            string += get_text(child)
-        return string
+        if self.annotation_head is not None:
+            return self.annotation_head
+        else:
+            string = ''
+            if self.parent_node.text:
+                string += self.parent_node.text
+            children = self.parent_node.getchildren()
+            for child in children:
+                if child.tag == '{urn:oasis:names:tc:opendocument:xmlns:office:1.0}annotation':
+                    break
+                string += get_text(child)
+            self.annotation_head = string
+        return self.annotation_head
 
 
 def extract_annotations(element_tree):
--- compare.py
+++ compare.py
@@ -1,23 +1,52 @@
-import odt_namespaces
-import annotation
 import difflib
 
-EXACT_MATCH_SIMILARITY = 0.6
-PARTLY_DELETION_SIMILARITY = 0.5
+import annotation
+import odt_namespaces
+
+
+EXACT_MATCH_SIMILARITY = 0.6  # similarity of substring
+PARTLY_DELETION_SIMILARITY = 0.5  # fuzzy similarity
 
 
 def compare(annotations_list, new_etree):
+    """ This function fine new parent for every annotation.
+
+    For every object that represent annotations we find and writewrite new parent as it's attribute
+    using fuzzy string comparison.
+
+    :param annotations_list: list of annotations.
+    :param new_etree: element tree of file with newer version.
+    :return: nothing.
+    """
     ratios = [0] * len(annotations_list)
     for p_node in new_etree.findall('.//text:p', odt_namespaces.namespaces):
         i = 0
         for annotation_node in annotations_list:
-            if difflib.SequenceMatcher(None, annotation_node.get_text(), annotation.get_text(p_node)).ratio() > ratios[i]:
-                ratios[i] = difflib.SequenceMatcher(None, annotation_node.get_text(), annotation.get_text(p_node)).ratio()
+            similarity = difflib.SequenceMatcher(None, annotation_node.get_text(), annotation.get_text(p_node)).ratio()
+            if similarity > ratios[i]:
+                ratios[i] = similarity
                 annotation_node.set_new_parent(p_node)
             i += 1
 
 
 def find_new_string(string, text):
+    """Find new commented fragment in parent`s text.
+
+    Used 3 different stages:
+
+    1 - function tries to find exact match of given string.
+    in success function ends work.
+    2 - function tries to find the longest substring of given string.
+    if length of substring good enough, function ends work.
+    3 - function uses partly_deletion() algorythm of finding new segment.
+    function ends work with result, if it is good enough.
+
+    :param string: original commented segment
+    :param text: text of nes parent for annotation
+
+    :return: set, that consists of 0 - start position in new text, 1 - length, 2 - similarity on success,
+             None in other case.
+    """
     if string is None or text is None:
         return None
     find_result = text.find(string)
@@ -36,17 +65,26 @@ def find_new_string(string, text):
 
 
 def partly_deletion(string, text):
+    """This function is our realisation of fuzzy substring search.
+
+    It deletes symbol from beginning and makes fuzzy comparison.
+    If result began better, it repeats this action until result lessen.
+    After tha, it return las deleted symbol on it`s place and do the same process but
+    starts from ending.
+
+    :param string: original commented segment
+    :param text: text of nes parent for annotation
+
+    :return: set, that consists of 0 - start position in new text, 1 - length, 2 - similarity
+    """
     similarity = 0
     off = 0
     last_char = None
-    # print('String:', string)
-    # print('Text:', text)
     sm = difflib.SequenceMatcher(None, string, text, False)
     while sm.ratio() > similarity:
         similarity = sm.ratio()
         last_char = text[0]
         text = text[1:]
-        # print(len(text))
         off += 1
         sm.set_seq2(text)
     if last_char is not None:
@@ -58,7 +96,6 @@ def partly_deletion(string, text):
         similarity = sm.ratio()
         last_char = text[-1]
         text = text[:-1]
-        # print(len(text))
         sm.set_seq2(text)
     if last_char is not None:
         text = text + last_char
