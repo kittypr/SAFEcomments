0d233e9edd55511dd4ee4be9344ee9e3ff85dab8
jzhuk <julettazhuk@gmail.com>
2017-12-14 15:47:49 +0700

    Readme updated

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='SAFEcomments',
-    version='1.0',
+    version='1.1',
 
     description='Script to transfer annotations from one doc to another.',
     long_description=long_description,
